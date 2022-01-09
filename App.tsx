85642df32ee2aeacc5edcb1c24207ab62065567d
Juan Alvarado <jalvarado91@gmail.com>
2022-01-09 18:46:01 -0500

    Add shuffle button

--- App.tsx
+++ App.tsx
@@ -21,6 +21,7 @@ import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 import { createApiClient, EpisodeDTO } from "./apiClient";
 import { SoulectionLogo } from "./Icons";
 import * as Linking from "expo-linking";
+import { ShuffleButton } from "./SuffleButton";
 
 export const queryClient = new QueryClient();
 const apiClient = createApiClient();
@@ -73,21 +74,42 @@ function EpisodeScreen() {
 
   const { isLoading, data: episodes, error } = useEpisodes();
 
+  function onRandomClick() {
+    if (episodes) {
+      const randomIndex = Math.floor(Math.random() * episodes.length);
+      onPlay(episodes[randomIndex]);
+    }
+  }
+
   return isLoading ? (
     <></>
   ) : (
-    <SafeAreaView style={tailwind("flex h-full")}>
+    <SafeAreaView style={tailwind("flex h-full bg-white relative")}>
       <AppBar offset={offset} />
       {episodes && (
-        <FlatList
-          style={tailwind("h-full pt-2 pb-10 flex-1")}
-          contentContainerStyle={tailwind("pb-4")}
-          data={episodes}
-          renderItem={(epItem) => (
-            <Episode onPlay={() => onPlay(epItem.item)} episode={epItem.item} />
-          )}
-          keyExtractor={(ep) => ep._id}
-        />
+        <>
+          <FlatList
+            style={tailwind("h-full pt-2 pb-10 flex-1")}
+            contentContainerStyle={tailwind("pb-4")}
+            data={episodes}
+            renderItem={(epItem) => (
+              <Episode
+                onPlay={() => onPlay(epItem.item)}
+                episode={epItem.item}
+              />
+            )}
+            keyExtractor={(ep) => ep._id}
+          />
+          <View
+            style={[
+              tailwind(
+                "absolute bg-transparent right-0 bottom-0 mb-8 mr-5 z-10"
+              ),
+            ]}
+          >
+            <ShuffleButton onClick={onRandomClick} />
+          </View>
+        </>
       )}
     </SafeAreaView>
   );
